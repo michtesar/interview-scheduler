8af3533edbc26b59ae6de3812958c0aa7df07ab8
Michael Tesar <michtesar@gmail.com>
2024-01-07 17:06:20 +0100

    Add first name and last name into ApplicationUser.cs

--- InterviewScheduler/Data/ApplicationUser.cs
+++ InterviewScheduler/Data/ApplicationUser.cs
@@ -5,4 +5,9 @@ namespace InterviewScheduler.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [PersonalData]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    public string LastName { get; set; }
 }
